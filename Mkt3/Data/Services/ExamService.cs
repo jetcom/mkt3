05f112bbc3d787d9d02b1108fb617033e2b9fad5
Jeremy Brown <jsb@jetcom.org>
2023-01-30 21:11:22 -0500

    Left Navbar now loads from the DB.  Still need to find a good way to add and remove sections

--- Mkt3/Data/Services/ExamService.cs
+++ Mkt3/Data/Services/ExamService.cs
@@ -7,6 +7,13 @@ public class ExamService
 {
    private bool Loading;
 
+   public async Task<List<Exam>> getAllExams()
+   {
+      await using var ctx = new Mkt3Context();
+      var exams = ctx.Exams.OrderBy(e=>e.courseNumber).ToList();
+      return exams;
+   }
+
    
    public async Task<Exam?> getExam(string courseID, string examID)
    {
